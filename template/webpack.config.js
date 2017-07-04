9d55c619b9dd294a19ba957229c5ccc61614e5fa
小青年 <1028317108@qq.com>
2017-07-04 23:33:18 +0800

    Update webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].[hash].js',
-		    publicPath: '/'
+        publicPath: '/'
     },
     module: {
         rules: [
